bd856b1006be1c052372d601444818c1e608689e
kmundil <kelliejmundil@gmail.com>
2021-02-16 17:30:16 +0000

    Added a loop to ask user questions

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,33 +3,39 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = input.question("What is your name? " );
+let candidateName = input.question("What is your name? ")
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
-let candidateAnswer = " ";
-let questions = ['Who was the first American woman in space?', 'True or false: 5000 meters = 5 kilometers.', '(5 + 3)/2 * 10 = ?', 'Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?', 'What is the minimum crew size for the International Space Station (ISS)?'];
-let correctAnswers = ['Sallly Ride', 'True', '40', 'Trajectory', '3'] ;
-let candidateAnswers;
+let candidateAnswer = input.question("Who was the first American woman in space? ")
+let questions = ['Who was the first American woman in space? ', 'True or false: 5000 meters = 5 kilometers. ', '(5 + 3)/2 * 10 = ? ', `Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ` , 'What is the minimum crew size for the International Space Station (ISS)? '];
+let correctAnswers = ['Sally Ride', 'True', '40', 'Trajectory', '3'] ;
+let candidateAnswers = 
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
 const input = require('readline-sync');
 
-
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  const input = require('readline-sync');
-let candidateAnswer = input.question("Who was the first American woman in space? ")
+
+for (let i = 0; i < questions.length; i++){ 
+  candidateAnswers = (input.question(questions[i]))
+  console.log(candidateAnswers);
+ 
+}
+
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+console.log(`Your Answer: ${candidateAnswers}`)
+
 if (candidateAnswer === correctAnswer) {
   console.log("Correct")
 }
