5a874d94b78f8c7cfdce8044867730189ad2380e
kmundil <kelliejmundil@gmail.com>
2021-02-20 16:52:59 +0000

    Added new line

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,7 +8,7 @@ let candidateName = input.question("What is your name? " );
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = [ 'Who was the first American woman in space? ', 'True or false: 5000 meters = 5 kilometers. ', '(5 + 3)/2 * 10 = ? ', 'Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2?', 'What is the minimum crew size for the ISS? ' ];
+let questions = [ 'Who was the first American woman in space? ', 'True or false: 5000 meters = 5 kilometers. ', '(5 + 3)/2 * 10 = ? ', 'Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2? ', 'What is the minimum crew size for the ISS? ' ];
 let correctAnswers = [ 'Sally Ride', 'True', '40', 'Trajectory', '3' ] ;
 let candidateAnswers = [];
 
@@ -40,7 +40,7 @@ for (let i = 0; i < questions.length; i++){
   if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()){
     gradeTotal+=1
     }
-   console.log(`${([i+1])}) ${questions[i]}\n Your Answer: ${candidateAnswers[i]} \n Correct Answer: ${correctAnswers[i]}`)
+   console.log(`${([i+1])}) ${questions[i]}\n Your Answer: ${candidateAnswers[i]} \n Correct Answer: ${correctAnswers[i]}\n`)
    
    } 
    let grade = (gradeTotal / questions.length) * 100
